9ca56bb0f070cdcdde9352484f273037ae130620
David Ward <david.a.ward@duke.edu>
2021-02-22 12:29:25 -0500

    Example of second default value for parameters

--- database_class.py
+++ database_class.py
@@ -1,6 +1,6 @@
 class Patient:
     
-    def __init__(self, first_name, last_name, age=None):
+    def __init__(self, first_name, last_name, age=None, state="NC"):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age

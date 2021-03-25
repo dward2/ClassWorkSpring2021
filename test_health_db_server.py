6637600ace8f8cc74ae2c037fb5f2ebc0c3fd337
David Ward <david.a.ward@duke.edu>
2021-03-25 12:00:31 -0400

    2. Import db

--- test_health_db_server.py
+++ test_health_db_server.py
@@ -1,6 +1,13 @@
 def test_add_patient_to_db():
     from health_db_server import add_patient_to_db
+    from health_db_server import db
     name = "David Ward"
     id = 100
     blood_type = "O+"
     add_patient_to_db(name, id, blood_type)
+    last_patient_in_db = db[-1]
+    expected = {"name": name,
+                "id": id,
+                "blood_type": blood_type,
+                "test": []}
+    assert last_patient_in_db == expected

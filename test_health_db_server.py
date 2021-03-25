dc263c684e034128d0eb44315d036d9284ed99ec
David Ward <david.a.ward@duke.edu>
2021-03-25 12:04:02 -0400

    3. Test get from database

--- test_health_db_server.py
+++ test_health_db_server.py
@@ -11,3 +11,15 @@ def test_add_patient_to_db():
                 "blood_type": blood_type,
                 "test": []}
     assert last_patient_in_db == expected
+
+
+def test_get_patient_from_db():
+    from health_db_server import get_patient_from_db
+    from health_db_server import db
+    test_patient = {"name": "Erica Emerson",
+                    "id": 200,
+                    "blood_type": "O-",
+                    "test": []}
+    db.append(test_patient)
+    answer = get_patient_from_db(200)
+    assert answer == test_patient

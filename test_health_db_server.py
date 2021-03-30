81ffbc94424afbd2551add014a85881e6fe466e2
David Ward <david.a.ward@duke.edu>
2021-03-30 16:08:48 -0400

    Add patient is now saved in MongoDB

--- test_health_db_server.py
+++ test_health_db_server.py
@@ -1,37 +1,37 @@
+from health_db_server import init_server
+
+init_server()
+
+
 def test_add_patient_to_db():
     from health_db_server import add_patient_to_db
-    from health_db_server import db
     name = "David Ward"
     id = 100
     blood_type = "O+"
-    add_patient_to_db(name, id, blood_type)
-    last_patient_in_db = db[-1]
-    expected = {"name": name,
-                "id": id,
-                "blood_type": blood_type,
-                "test": []}
-    assert last_patient_in_db == expected
+    answer = add_patient_to_db(name, id, blood_type)
+    answer.delete()
+    assert answer.id_no == id
 
 
-def test_get_patient_from_db():
-    from health_db_server import get_patient_from_db
-    from health_db_server import db
-    test_patient = {"name": "Erica Emerson",
-                    "id": 200,
-                    "blood_type": "O-",
-                    "test": []}
-    db.append(test_patient)
-    answer = get_patient_from_db(200)
-    assert answer == test_patient
-
-
-def test_get_patient_from_db_missing():
-    from health_db_server import get_patient_from_db
-    from health_db_server import db
-    test_patient = {"name": "Erica Emerson",
-                    "id": 201,
-                    "blood_type": "O-",
-                    "test": []}
-    db.append(test_patient)
-    answer = get_patient_from_db(200)
-    assert answer is False
+# def test_get_patient_from_db():
+#     from health_db_server import get_patient_from_db
+#     from health_db_server import db
+#     test_patient = {"name": "Erica Emerson",
+#                     "id": 200,
+#                     "blood_type": "O-",
+#                     "test": []}
+#     db.append(test_patient)
+#     answer = get_patient_from_db(200)
+#     assert answer == test_patient
+#
+#
+# def test_get_patient_from_db_missing():
+#     from health_db_server import get_patient_from_db
+#     from health_db_server import db
+#     test_patient = {"name": "Erica Emerson",
+#                     "id": 201,
+#                     "blood_type": "O-",
+#                     "test": []}
+#     db.append(test_patient)
+#     answer = get_patient_from_db(200)
+#     assert answer is False
